d6285b25a9d48cbe955c9620a033f6766a06c863
Kevin Moore <moore.kevin.p@gmail.com>
2020-05-27 15:38:14 -0400

    ophidian bank completed

--- QueueDrills.js
+++ QueueDrills.js
@@ -76,5 +76,55 @@ function stackQueue() {
 stackQueue();
 
 //9. Square dance pairings
+const dancers = new Queue();
+dancers.enqueue({ gender: 'F', name: 'Jane' });
+dancers.enqueue({ gender: 'M', name: 'Frank' });
+dancers.enqueue({ gender: 'F', name: 'Madonna' });
+dancers.enqueue({ gender: 'M', name: 'David' });
+dancers.enqueue({ gender: 'F', name: 'Beyonce' });
+dancers.enqueue({ gender: 'M', name: 'Chris' });
+dancers.enqueue({ gender: 'M', name: 'Sherlock' });
+dancers.enqueue({ gender: 'M', name: 'John' });
+
+function squareDancePairings(queue) {
+  const men = new Queue;
+  const women = new Queue;
+  while(queue.first !== null) {
+    let nextUp = queue.first.value;
+    queue.dequeue();
+
+    if(nextUp.gender === 'F') {
+      women.enqueue(nextUp.name);
+    } else {
+      men.enqueue(nextUp.name);
+    }
+    if(men.first !==null && women.first !==null) {
+      const pairings = `${men.first.value} is paired with ${women.first.value}`;
+      console.log(pairings);
+      men.dequeue();
+      women.dequeue();
+    }
+  }
+}
+//squareDancePairings(dancers);
 
 //10. The Ophidian Bank
+function ophidianBank() {
+  const bankLine = new Queue();
+  for(let i =0; i <10; i++) {
+    console.log('A person joined the line'); 
+    bankLine.enqueue('A person');
+  }
+  const person = bankLine.dequeue();
+  if(Math.random() < 0.25) {
+    console.log('Person sent to back of the queue');
+    bankLine.enqueue(person);
+  } else {
+    console.log('This person had all the right paperwork');
+  }
+}
+ophidianBank();
+ophidianBank();
+ophidianBank();
+ophidianBank();
+ophidianBank();
